e992ee347351e651cfebb6a85326a5e5e11eb710
diyandim <diyanmardimitrov@gmail.com>
2025-05-06 19:22:46 +0300

    added button for reset of the grid along with instructions on how to play

--- javascript.js
+++ javascript.js
@@ -1,11 +1,30 @@
 function createGrid(rows, colums){
     const content = document.querySelector("#container");
 
+    container.style.gridTemplateColumns = `repeat(${colums}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
     for (let i = 0; i < (rows * colums); i++){
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+    
+
+        square.addEventListener(
+            "mouseover",
+            (event) => {
+                event.target.style.backgroundColor = "black";
+            },
+        );
     }
 }
-
 createGrid(16, 16);
+
+const mybutton = document.querySelector('#btn');
+
+mybutton.addEventListener("click", () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = '';
+    });
+});
